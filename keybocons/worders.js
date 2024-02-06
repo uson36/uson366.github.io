246c30ee86befbd44e9028f1ed89d1d480dbf4d3
uson36 <105691565+uson36@users.noreply.github.com>
2024-02-06 16:00:44 +0900

    Delete keybocons directory

--- keybocons/worders.js
+++
@@ -1,8 +0,0 @@
-export let dictioMan = ['under','use','power','delicious','problem','produce','this','that','train',
-'fully','let','handle','read','bleed','crack','crime','vibrate','sound','real','brave','like','however',
-'dirty','clean','house','another','grace','love','queue','mouse','bat','super','mobile','novel','funny',
-'police','prime','vase','baseline','cybersecurity','job','marinate','earth','crabs','smile','trade','',
-'lion','tiger','bird','giant','never','always','usage','uranium','bomb','guns','rifle','bench','security',
-'leak','fifth','match','goods','heist','loses','armed','backroom','operations','project','task','force',
-'combat','weapon','tanks','fighter','jets','nukes'];
-export let dictioManLength = dictioMan.length;
